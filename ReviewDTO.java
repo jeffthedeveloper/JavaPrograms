bfd07529b7845bedfd41e5728840db8e22cd77ee
Jefferson Firmino Mendes <110995133+jeffthedeveloper@users.noreply.github.com>
2025-04-04 15:19:48 -0300

    Add files via upload

---
+++ ReviewDTO.java
@@ -0,0 +1,68 @@
+package com.devsuperior.movieflix.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.NotBlank;
+
+import com.devsuperior.movieflix.entities.Review;
+
+public class ReviewDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	
+	@NotBlank(message = "Não é permitido avaliações em branco")
+	private String text;
+	private Long movieId;
+	private UserDTO user;
+	
+	public ReviewDTO() {
+	}
+
+	public ReviewDTO(Long id, String text, Long movieId, UserDTO user) {
+		this.id = id;
+		this.text = text;
+		this.movieId = movieId;
+		this.user = user;
+	}
+	
+	public ReviewDTO(Review review) {
+		MovieDTO movie = new MovieDTO(review.getMovie());
+		id = review.getId();
+		text = review.getText();
+		movieId = movie.getId();
+		user = new UserDTO(review.getUser());
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public Long getMovieId() {
+		return movieId;
+	}
+
+	public void setMovieId(Long movieId) {
+		this.movieId = movieId;
+	}
+
+	public UserDTO getUser() {
+		return user;
+	}
+
+	public void setUser(UserDTO user) {
+		this.user = user;
+	}
+}
